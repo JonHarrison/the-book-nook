6d52b28a3bafddb49d6d0c3fedf8d4f68b8dd81d
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-14 15:26:59 +0000

    add home, login and signup components

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+// from https://github.com/wisdomekpotu/react18-firebasev9-auth
+
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import { useUserAuth } from "../context/userAuthContext";
+
+const Home = () => {
+  const { logOut, user } = useUserAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="p-4 box mt-3 text-center">
+        Hello Welcome <br />
+      </div>
+      <div className="d-grid gap-2">
+        <Button variant="primary" onClick={handleLogout}>
+          Log out
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
