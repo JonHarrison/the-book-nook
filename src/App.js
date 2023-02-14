490671ecb41af5382431ad8597818a76419884c4
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-14 16:03:14 +0000

    app.js - add routes with firebase authentication and login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { Routes, Route } from 'react-router-dom'
+import { Container, Header, Row, Col } from 'react-bootstrap'
+
+import { UserAuthContextProvider } from './context/userAuthContext';
+
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <p>The Book Nook</p>
-      </header>
+      </div>
+      <Container style={{ width: "400px" }}>
+        <Row>
+          <Col>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </UserAuthContextProvider> 
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
