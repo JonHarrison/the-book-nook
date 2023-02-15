90a9b8cbf74891e9689bb94f14f8dad1bb09f270
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-15 17:25:35 +0000

    App.js - add Header, Main and Footer components and Route to /

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+
+// Components
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+// Style
+import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>The Book Nook</p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
