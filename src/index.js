cd3259c70de770e8afa8b2e8f21f2abd3fe27440
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-15 17:21:20 +0000

    index.js - import bootstrap css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 
 import App from './App';
