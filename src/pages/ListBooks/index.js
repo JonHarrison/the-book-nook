f18e23d63a8a0818cfba6851953dad4ae8fec9d1
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-15 12:41:41 +0000

    ListBooks - update component for Google Books API

--- src/pages/ListBooks/index.js
+++ src/pages/ListBooks/index.js
@@ -1,13 +1,42 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useFetch } from '../../hooks/useFetch'
 
 import './style.css'
 
 export default function ListBooks() {
-    const [url, setUrl] = useState('https://gutendex.com/books/')
-    const { data: books, isPending, error } = useFetch(url)
+
+    const [searchResults, setSearchResults] = useState(32);
+    const [searchIndex, setSearchIndex] = useState(0);
+
+    function generateURL() { return ( `https://www.googleapis.com/books/v1/volumes?q=all&maxResults=${searchResults}&startIndex=${searchIndex}` ); }
+
+    const [url, setUrl] = useState(generateURL())
+    const { data: books, isPending, error } = useFetch(generateURL())
+
+    useEffect(() => {
+            console.log(`calling setURL with searchIndex=${searchIndex}`)
+            setUrl(generateURL())
+    }, [searchIndex]) // useEffect updates when searchIndex is changed
 
     console.log(books)
+
+    const prev = () => {
+        console.log(`prev searchIndex=${searchIndex} searchResults=${searchResults}`)
+        if (searchIndex >= searchResults)
+        {
+            let newIndex = searchIndex - searchResults;
+            setSearchIndex(newIndex);
+        }
+    }
+
+    const next = (total) => {
+        console.log(`next total=${total} searchIndex=${searchIndex} searchResults=${searchResults}`)
+        if (searchIndex <= total-searchResults)
+        {
+            let newIndex = searchIndex + searchResults;
+            setSearchIndex(newIndex)
+        }
+    }
 
     return (
         <div>
@@ -15,15 +44,15 @@ export default function ListBooks() {
             {isPending && <div>Loading books...</div>}
             {error && <div>{error}</div>}
             <div className="book-page-nav">
-                <button class="btn" onClick={() => setUrl(books.previous)}>Previous</button>
-                <button class="btn" onClick={() => setUrl(books.next)}>Next</button>
+                <button className="btn" onClick={() => prev()}>Previous</button>
+                <button className="btn" onClick={() => next(books.totalItems)}>Next</button>
             </div>
-            <p>Count : {books && books.count}</p>
+            <p>Count : {books && books.totalItems}</p>
             <div className="book-list">
-                {books && books.results.map(book => (
+                {books && books.items.map(book => (
                     <div className="book" key={book.id}>
-                        <h3 className="book-title">{book.title}</h3>
-                        <img className="book-img" src={book.formats['image/jpeg']} alt={book.title}></img>
+                        <h3 className="book-title">{book.volumeInfo.title}</h3>
+                        <img className="book-img" src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}></img>
                     </div>
                 ))}
             </div>
