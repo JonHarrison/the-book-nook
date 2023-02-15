45deaa941396f90ec3ebaa8422b88d5fe56c070d
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-15 17:24:39 +0000

    add boilerplate Header, Footer, Main components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+const Footer = () => {
+  return (
+    <footer className="App-footer" id="footer">Footer</footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+const Header = () => {
+  return (
+    <header className="App-header" id="App-header">Header</header>
+  )
+}
+
+export default Header
