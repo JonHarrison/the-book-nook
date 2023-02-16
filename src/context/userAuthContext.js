be24ccda8708e449942c2005ad392319e6e3b0c3
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-16 11:43:56 +0000

    comment auth context

--- src/context/userAuthContext.js
+++ src/context/userAuthContext.js
@@ -12,14 +12,20 @@ import {
 
 import { auth } from "../config/firebase";
 
+// create Context.API object for this user authentication object
 const userAuthContext = createContext();
 
+// provide context for authentication, used to wrap the App components
 export function UserAuthContextProvider({ children }) {
+  // current user
   const [user, setUser] = useState({});
 
+  // login with email and password
   function logIn(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
   }
+
+  // create a new user with email, password and display name
   function signUp(email, password, name) {
     createUserWithEmailAndPassword(auth, email, password)
       .then(createdUser => {
@@ -33,19 +39,26 @@ export function UserAuthContextProvider({ children }) {
       console.log(err);
     })
   }
+
+  // logout
   function logOut() {
     return signOut(auth);
   }
+
+  // sign-in with Google authentication
   function googleSignIn() {
     const googleAuthProvider = new GoogleAuthProvider();
     return signInWithPopup(auth, googleAuthProvider);
   }
+
+  // sign-in with Facebook authentication
   function facebookSignIn() {
     const facebookAuthProvider = new FacebookAuthProvider();
     return signInWithPopup(auth, facebookAuthProvider);
   }
 
   useEffect(() => {
+    // handle authentication state changed events
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
         // User is signed in
@@ -63,9 +76,10 @@ export function UserAuthContextProvider({ children }) {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, []); // runs once
 
   return (
+    // make the following methods available to child components
     <userAuthContext.Provider
       value={{ user, logIn, signUp, logOut, facebookSignIn, googleSignIn }}
     >
@@ -74,6 +88,7 @@ export function UserAuthContextProvider({ children }) {
   );
 }
 
+// export context to allow the App to access the current user, login and out etc.
 export function useUserAuth() {
   return useContext(userAuthContext);
 }
