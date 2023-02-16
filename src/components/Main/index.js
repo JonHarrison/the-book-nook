37488d650c497e374d02050b488ca56e6857b89c
Ansar Mehmood <ansar.mehmood@directlinegroup.co.uk>
2023-02-16 22:47:57 +0300

    added logo, updated  index.js and style.css in main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 import './style.css'
 
 const Main = () => {
+
   return (
-    <main className="App-main" id="main">Main</main>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="mx-auto my-5 col-lg-2 col-md-4 col-xs-12" >
+          <img class="img-fluid rounded " src={logo} />
+        </div>        
+        <div className="mx-auto col" >
+        <div className='headers'>
+          <h1>The Book Nook</h1>
+          <h2> the book galary </h2>
+          </div>
+          </div>
+      </div>
+    </div>
+  );}
+
 
 export default Main
