bf6c2c6a6afb878a83a26b5414c5bd7a9f774f41
Jade <jadecourtney92@gmail.com>
2023-02-16 13:16:17 +0000

    Added a new folder, placed page folders and index and css for library. Next step - amend code to suit folders and add carousel.

---
+++ src/Pages/Library/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+const Library = () => {
+  return (
+    <main className="App-Library" id="Library">Personal Library</main>
+  )
+}
+
+export default Main
