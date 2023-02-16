b5d5bcc53429565899a6182d6f2f0aabab1aa175
Jade <jadecourtney92@gmail.com>
2023-02-16 20:55:30 +0000

    Added a row of 4 cards, installed holderjs so images display>

--- src/Pages/SearchArea/index.js
+++ src/Pages/SearchArea/index.js
@@ -2,3 +2,64 @@ import React from 'react'
 
 import './style.css'
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import 'holderjs';
+
+function SearchArea() {
+  return (
+    <div className= "container">
+      <div class="row" >
+        <div class="App-searchArea col-lg-12 col-md-12 col-sm-12">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default SearchArea;
