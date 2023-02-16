2d04edbe13c60d8c9ec0759c1b6f12dd61cd3635
Jade <jadecourtney92@gmail.com>
2023-02-16 20:04:50 +0000

    Added react bootstrap carousel and added a root to APS. Currently not displaying properly - need to return to this and fix later.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Library from './Pages/Library'
+import SearchArea from './Pages/SearchArea'
 
 // Style
 import './App.css'
@@ -14,6 +16,8 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/Library" element={<Library />} />
+        <Route path="/SearchArea" element={<SearchArea />} />
       </Routes>
       <Footer />
     </>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,9 @@ const Footer = () => {
   return (
     <footer className="App-footer" id="footer">
       <div className= "row">
-      <div className = "col-lg-12 col-md-12 col-sm-12">&copy; Copyright <strong>Created by Jon, Ansar, Jade & Iain</strong> Contact Us: <SocialIcon url="https://github.com/JonHarrison/the-book-nook.git" /></div>
+      <div className = "col-lg-12 col-md-12 col-sm-12 copyright">&copy; Copyright </div>
+      <p><strong>Created by Jon, Ansar, Jade & Iain</strong></p> 
+      <p>Contact Us: <SocialIcon url="https://github.com/JonHarrison/the-book-nook.git" /></p>
       </div>
     </footer>
   )
