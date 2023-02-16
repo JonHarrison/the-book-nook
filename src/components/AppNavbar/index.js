d28db2e5f1408b64d7bfde0af78f66edeadc279f
DESKTOP-6TQQDEH\nergal <loki@mail.com>
2023-02-16 21:00:00 +0000

    resized logo

--- src/components/AppNavbar/index.js
+++ src/components/AppNavbar/index.js
@@ -1,16 +1,20 @@
-import {Navbar, Nav, Container } from "react-bootstrap"; 
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Logo from "../../assets/logos/BookStoreLogo.png"
+
+import "./style.css"
 
 const AppNavbar = () => {
-    return (
+  return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">The Book Nook</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={Logo} alt="Brand logo" />
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+
             <Nav.Link href="#features">Log in</Nav.Link>
             <Nav.Link href="#personal">Personal library</Nav.Link>
-            <Nav.Link href="#search">Search??</Nav.Link>
             <Nav.Link href="#contact">Contact us</Nav.Link>
           </Nav>
         </Container>
