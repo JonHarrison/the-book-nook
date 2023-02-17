236ea77bef98f89043b638b808c841f8a476e71f
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-17 16:39:12 +0000

    App.js - add UserAuthContext provider to wrap routes, ensure user must login before accessing app pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 
 import { Container, Header, Footer, Row, Col } from 'react-bootstrap'
 
+import { UserAuthContextProvider } from './context/userAuthContext'
+
 // Components
 import AppHeader from './components/AppHeader'
 import AppFooter from './components/AppFooter'
@@ -9,15 +11,27 @@ import AppFooter from './components/AppFooter'
 // Containers
 import Main from './containers/Main'
 
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 // Style
 import './App.css';
 
 const App = () => {
   return (
+    <div className="App">
       <AppHeader className="App-header" />
       <Main className="App-main">
-              <Routes>
-              </Routes>
+        <UserAuthContextProvider>
+          <Routes>
+            <Route exact path="/" element={<Home />} /> {/* Login */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/home" element={<Home />} /> {/* replace with library */}
+          </Routes>
+        </UserAuthContextProvider>
       </Main>
       <AppFooter className="App-footer" />
     </div>
