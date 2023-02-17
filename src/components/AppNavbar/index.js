d04a017a9dc6bcbfb5c33c41fbebe4ba5c3b0bf5
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-17 06:32:07 +0000

    appnavbar - add dropdown to login and wrap links in LinkContainer

--- src/components/AppNavbar/index.js
+++ src/components/AppNavbar/index.js
@@ -1,17 +1,31 @@
-import {Navbar, Nav, Container } from "react-bootstrap"; 
+import {Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap"; 
+
+import { LinkContainer } from 'react-router-bootstrap'
 
 const AppNavbar = () => {
     return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">The Book Nook</Navbar.Brand>
+      <Navbar bg="dark" variant="dark" fixed="top" id="navbar">
+        <Container fluid>
+          <LinkContainer to="/">
+            <Navbar.Brand>The Book Nook</Navbar.Brand>
+          </LinkContainer>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Log in</Nav.Link>
             <Nav.Link href="#personal">Personal library</Nav.Link>
             <Nav.Link href="#search">Search??</Nav.Link>
             <Nav.Link href="#contact">Contact us</Nav.Link>
+            <NavDropdown title="Log in" id="basic-nav-dropdown">
+              <LinkContainer to="/login">
+                <NavDropdown.Item >
+                  <Badge pill bg="primary">Login</Badge>
+                </NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="/signup">
+                <NavDropdown.Item >Sign up</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
