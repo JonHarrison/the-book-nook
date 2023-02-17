afd86400e11d00b706aa8f64b3b0ad7936348b72
Jade <jadecourtney92@gmail.com>
2023-02-17 09:46:27 +0000

    COmmented out old card code in case needed for future use. Implemented grid card code.

--- src/Pages/SearchArea/index.js
+++ src/Pages/SearchArea/index.js
@@ -4,62 +4,92 @@ import './style.css'
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import 'holderjs';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-function SearchArea() {
+function GridExample() {
   return (
-    <div className= "container">
-      <div class="row" >
-        <div class="App-searchArea col-lg-12 col-md-12 col-sm-12">
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+    <Row xs={1} md={4} className="g-4">
+      {Array.from({ length: 1 }).map((_, idx) => (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Card title</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
             </Card.Body>
+            <Button variant="primary">Go somewhere</Button>
           </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
-    </div>
-
+        </Col>
+      ))}
+    </Row>
   );
 }
 
-export default SearchArea;
+export default GridExample;
+
+//Old code commented out for now
+
+// import 'holderjs';
+
+// function SearchArea() {
+//   return (
+//     <div className= "container">
+//       <div class="row" >
+//         <div class="App-searchArea col-lg-12 col-md-12 col-sm-12">
+//           <Card style={{ width: '18rem' }}>
+//             <Card.Img variant="top" src="holder.js/100px180" />
+//             <Card.Body>
+//               <Card.Title>Card Title</Card.Title>
+//               <Card.Text>
+//                 Some quick example text to build on the card title and make up the
+//                 bulk of the card's content.
+//               </Card.Text>
+//               <Button variant="primary">Go somewhere</Button>
+//             </Card.Body>
+//           </Card>
+//           <Card style={{ width: '18rem' }}>
+//             <Card.Img variant="top" src="holder.js/100px180" />
+//             <Card.Body>
+//               <Card.Title>Card Title</Card.Title>
+//               <Card.Text>
+//                 Some quick example text to build on the card title and make up the
+//                 bulk of the card's content.
+//               </Card.Text>
+//               <Button variant="primary">Go somewhere</Button>
+//             </Card.Body>
+//           </Card>
+//           <Card style={{ width: '18rem' }}>
+//             <Card.Img variant="top" src="holder.js/100px180" />
+//             <Card.Body>
+//               <Card.Title>Card Title</Card.Title>
+//               <Card.Text>
+//                 Some quick example text to build on the card title and make up the
+//                 bulk of the card's content.
+//               </Card.Text>
+//               <Button variant="primary">Go somewhere</Button>
+//             </Card.Body>
+//           </Card>
+//           <Card style={{ width: '18rem' }}>
+//             <Card.Img variant="top" src="holder.js/100px180" />
+//             <Card.Body>
+//               <Card.Title>Card Title</Card.Title>
+//               <Card.Text>
+//                 Some quick example text to build on the card title and make up the
+//                 bulk of the card's content.
+//               </Card.Text>
+//               <Button variant="primary">Go somewhere</Button>
+//             </Card.Body>
+//           </Card>
+//         </div>
+//       </div>
+//     </div>
+
+//   );
+// }
+
+// export default SearchArea;
