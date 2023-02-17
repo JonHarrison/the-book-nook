ba79bb5ace5d3b37b1523ace4a9620606df2ac6f
Jade <jadecourtney92@gmail.com>
2023-02-17 09:51:12 +0000

    Updated function name and export as well as the styling so card sits on the left-hand side for now

--- src/Pages/SearchArea/index.js
+++ src/Pages/SearchArea/index.js
@@ -8,7 +8,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import 'holderjs';
 
-function GridExample() {
+function CardGrid() {
   return (
     <Row xs={1} md={4} className="g-4">
       {Array.from({ length: 1 }).map((_, idx) => (
@@ -31,7 +31,7 @@ function GridExample() {
   );
 }
 
-export default GridExample;
+export default CardGrid;
 
 //Old code commented out for now
 
