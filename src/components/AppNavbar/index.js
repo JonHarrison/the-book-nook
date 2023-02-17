3316925a804b486bbd471d691ccbad7038db5d03
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-17 17:55:41 +0000

    AppNavbar - removed NavDropdown import as it is not used

--- src/components/AppNavbar/index.js
+++ src/components/AppNavbar/index.js
@@ -1,4 +1,4 @@
-import { Navbar, Nav, NavLink, NavDropdown, Container, OverlayTrigger, Tooltip, Badge } from "react-bootstrap"
+import { Navbar, Nav, NavLink, Container, OverlayTrigger, Tooltip, Badge } from "react-bootstrap"
 
 import { LinkContainer } from 'react-router-bootstrap'
 
