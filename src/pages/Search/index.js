49fb6a36f9ba73b8434e787c3ae390bc0a9c2ac7
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-19 10:21:41 +0000

    Search - start with no books and landing string instead

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -10,7 +10,7 @@ const Search = (props) => {
 
     const APIKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
 
-    const [url, setUrl] = useState(`https://www.googleapis.com/books/v1/volumes?q=maxResults=40&key=${APIKey}`)
+    const [url, setUrl] = useState()
 
     const { data: books, isPending, error } = useFetch(url) // the useEffect in the useFetch updates the hook when the url changes
 
@@ -25,6 +25,7 @@ const Search = (props) => {
         <div className="App-search">
             <div className="App-search-book-list">
                 {isPending && <div>Loading books...</div>}
+                {!url && <h1>Start your book search here >>></h1>}
                 {error && <div>{error}</div>}
                 <BookList books={books} />
             </div>
