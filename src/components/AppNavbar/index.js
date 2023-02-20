1f33887f2ac1a33cebce03e9fe6e92516b1d9669
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-20 23:15:08 +0000

    AppNavbar - update login button to show login / welcome + logout depending upon isLoggedIn state

--- src/components/AppNavbar/index.js
+++ src/components/AppNavbar/index.js
@@ -49,7 +49,6 @@ const AppNavbar = () => {
   console.log('User - ', user);
 
   return (
-    <>
     <div>
       <Navbar bg="dark" variant="dark" expand="lg" stick="top" id="navbar">
         <Container fluid>
@@ -64,19 +63,21 @@ const AppNavbar = () => {
               <Nav.Link href="Library">Your Book Nook</Nav.Link>
               <Nav.Link href="Search">Search</Nav.Link>
               <Nav.Link href="Contact">Contact us</Nav.Link>
-              <LinkContainer to="/login">
-                <NavLink>
+              {isLoggedIn ?
+                <>
+                  {user && <p>Welcome {user.displayName} </p>}
+                  <Button variant="primary" onClick={handleLogout}>Logout</Button>
+                </>
+                :
                   <OverlayTrigger placement="bottom" delay={{ show: 250, hide: 400 }} overlay={<Tooltip>Click to login</Tooltip>}>
-                    <Badge pill bg="light" text="dark">Login</Badge>
+                    <Button onClick={handleLogin}>Login</Button>
                   </OverlayTrigger>
-                </NavLink>
-              </LinkContainer>
+              }
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       </div>
-    </>
   )
 }
 
