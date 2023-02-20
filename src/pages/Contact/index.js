d159e4fbfe0c065c223c734c8e4242a3576d94be
Ansar Mehmood <ansar.mehmood@directlinegroup.co.uk>
2023-02-20 16:53:46 +0000

    initail code added for contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,9 +4,40 @@ import './style.css'
 
 const ContactPage = () => {
   return (
-<div>
-  <h1>Contact Page</h1>
-  </div>
+    <div>
+      <h1>Contact Page</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <h4 class="text-uppercase m-0">Address</h4>
+                <hr class="my-4" />
+                <div class="small text-black-50">12132 gggh street, England</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <h4 class="text-uppercase m-0">Email</h4>
+                <hr class="my-4" />
+                <div class="small text-black-50"><a href="#!">11233@gmail.com</a></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <h4 class="text-uppercase m-0">Phone</h4>
+                <hr class="my-4" />
+                <div class="small text-black-50">+44 (800) 45454885 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
