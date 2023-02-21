ab68b5b3c54de265981724208aa2ef7be11de7fe
lokihiem <loki@mail.com>
2023-02-21 14:41:53 +0000

    changes

--- src/components/AppNavbar/index.js
+++ src/components/AppNavbar/index.js
@@ -14,7 +14,7 @@ const AppNavbar = () => {
 
   return (
     <div>
-      <Navbar className="App-navbar" bg="dark" variant="dark" expand="lg" stick="top" id="navbar">
+      <Navbar  className="App-navbar" expand="lg" sticky="top" id="navbar">
         <Container fluid>
           <LinkContainer to="/">
             <Navbar.Brand>
@@ -24,10 +24,15 @@ const AppNavbar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="App-navbar-links ms-auto">
-              <Nav.Link href="Library">Your Book Nook</Nav.Link>
-              <Nav.Link href="Search">Search</Nav.Link>
-              <Nav.Link href="Contact">Contact us</Nav.Link>
-              <AppLogin />
+              <Nav.Link className="nav-text" href="#personal">Personal library</Nav.Link>
+              <Nav.Link className="nav-text" href="#contact">Contact us</Nav.Link>
+              <LinkContainer to="/login">
+                <NavLink>
+                  <OverlayTrigger placement="bottom" delay={{ show: 250, hide: 400 }} overlay={<Tooltip>Click to login</Tooltip>}>
+                    <Badge pill bg="light" text="dark">Login</Badge>
+                  </OverlayTrigger>
+                </NavLink>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
