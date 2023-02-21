3ebbcc5b663adaa43c30c4c9dcb2c4266e80b70f
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-21 09:11:19 +0000

    Signup - change title

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
   return (
     <>
       <div className="p-4 box">
-        <h2 className="mb-3">Firebase Auth Signup</h2>
+        <h2 className="mb-3">The Book Nook Signup</h2>
         {error && <Alert variant="danger">{error}</Alert>}
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicName">
