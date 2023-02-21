1a8cb2f1be9d9bb89d709b2edf43ace4e18ef460
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-21 17:57:33 +0000

    firebase config - refactor into firebase and firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,5 @@
-// import * as firebase from 'firebase'
-import { initializeApp, getApps } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from "firebase/auth"
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -16,15 +10,4 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
-console.log(firebaseConfig);
-
-// Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
-
-const analytics = getAnalytics(app);
-
-export const db = getFirestore(app);
-
-export const auth = getAuth(app);
-
-export default app
+console.log(firebaseConfig)
