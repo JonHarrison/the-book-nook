57b042c262cd9b52807441c6c64b2c16afcacd38
Jade <jadecourtney92@gmail.com>
2023-02-22 23:03:12 +0000

    Updated footer with just social media icons, removed CSS for github icon as appeared not to be needed.

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
 import { SocialIcon } from 'react-social-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 import './style.css'
 
 const AppFooter = () => {
   return (
     <footer className="App-footer" id="footer" sticky="bottom">
-      <div>&copy; Copyright<strong>Created by Jon, Ansar, Jade & Iain</strong></div>
-      <div>Contact Us:<SocialIcon url="https://github.com/JonHarrison/the-book-nook.git" style={{ height: 32, width: 32 }} /></div>
+      <div>&copy; Copyright<strong>Created by Jon, Ansar, Jade & Iain.</strong></div>
+      <div><SocialIcon url="https://github.com/JonHarrison/the-book-nook.git" style={{ height: 32, width: 32 }} /></div>
+      <div><a href="" className="iconLinks"><FontAwesomeIcon icon={faLinkedin} className="fa-2x "/></a></div>
+      <div><a href="" className="iconLinks"><FontAwesomeIcon icon={faFacebook} className="fa-2x "/></a></div>
     </footer>
   )
 }
