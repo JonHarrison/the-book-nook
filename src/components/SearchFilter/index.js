033677e548098146e073b010c19267a203e2647f
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-22 22:38:14 +0000

    SearchFilter - remove reference to useEffect from react and FormGroup, FormControl, InputGroup and Accordion from react-bootstrap

--- src/components/SearchFilter/index.js
+++ src/components/SearchFilter/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
-import { Button, Form, FormGroup, FormControl, InputGroup, Accordion } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 
 import './style.css';
 
